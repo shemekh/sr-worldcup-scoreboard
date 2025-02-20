ac0bf87123f0755832f1aa8b9891d9d49086247a
shemekh <przemek.brzozowski@yahoo.com>
2025-02-20 17:15:44 +0100

    Add FootballGame test cases

--- src/main/java/com/sr/scoreboard/game/FootballGame.java
+++ src/main/java/com/sr/scoreboard/game/FootballGame.java
@@ -2,6 +2,22 @@ package com.sr.scoreboard.game;
 
 public class FootballGame implements Game {
 
+  private final String homeTeam;
+  private final String awayTeam;
+  private int homeScore;
+  private int awayScore;
+
+  public FootballGame(String homeTeam, String awayTeam) {
+    this.homeTeam = homeTeam;
+    this.awayTeam = awayTeam;
+    this.homeScore = 0;
+    this.awayScore = 0;
+  }
+
+  public static FootballGame of(String homeTeam, String awayTeam) {
+    return new FootballGame(homeTeam, awayTeam);
+  }
+
   @Override
   public void updateScore(int homeScore, int awayScore) {
   }
@@ -11,4 +27,19 @@ public class FootballGame implements Game {
     return 0;
   }
 
+  public String getHomeTeam() {
+    return homeTeam;
+  }
+
+  public String getAwayTeam() {
+    return awayTeam;
+  }
+
+  public int getHomeScore() {
+    return homeScore;
+  }
+
+  public int getAwayScore() {
+    return awayScore;
+  }
 }
--- src/test/groovy/com/sr/scoreboard/game/FootballGameTest.groovy
+++ src/test/groovy/com/sr/scoreboard/game/FootballGameTest.groovy
@@ -4,15 +4,56 @@ import spock.lang.Specification
 
 class FootballGameTest extends Specification {
 
+  static HOME_TEAM = "Mexico"
+  static AWAY_TEAM = "Canada"
+
   def "should properly create FootballGame object"() {
+    when:
+      def game = FootballGame.of(HOME_TEAM, AWAY_TEAM)
+    then:
+      game.homeTeam == HOME_TEAM
+      game.awayTeam == AWAY_TEAM
+      game.homeScore == 0
+      game.awayScore == 0
+      game.totalScore == 0
   }
 
   def "should properly update score"() {
+    given:
+      def game = FootballGame.of(HOME_TEAM, AWAY_TEAM)
+    when:
+      game.updateScore(3, 1)
+    then:
+      game.homeScore == 3
+      game.awayScore == 1
+      game.totalScore == 4
   }
 
   def "should properly update score multiple times"() {
+    given:
+      def game = FootballGame.of(HOME_TEAM, AWAY_TEAM)
+    when:
+      game.updateScore(3, 1)
+      game.updateScore(3, 2)
+      game.updateScore(3, 3)
+      game.updateScore(4, 3)
+    then:
+      game.homeScore == 4
+      game.awayScore == 3
+      game.totalScore == 7
   }
 
   def "should throw exception while updating with negative score"() {
+    given:
+      def game = FootballGame.of(HOME_TEAM, AWAY_TEAM)
+    when:
+      game.updateScore(homeScore, awayScore)
+    then:
+      thrown(IllegalArgumentException)
+    where:
+      homeScore | awayScore
+      -1        | 2
+      3         | -5
+      -3        | -2
   }
 }
