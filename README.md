bef9479057f88aae5e8aa5510af5d7fc1b2ed425
shemekh <przemek.brzozowski@yahoo.com>
2025-02-20 19:56:06 +0100

    Add improved README and runnable configuration

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # WorldCup Scoreboard 
-## JDK distribution and version used during development
-```
-Azul Zulu@21.0.4
-```
 ## Assumptions taken/Comments
+1. No external libraries used apart of testing library.
+1. Operation from exercise _"4. Get a summary of games by total score [...]"_ is implemented by using
+LinkedHashMap to maintain insertion order and sorting by total goals as requested.
+
 ## How to use
+To run tests
+```
+./gradlew test
+```
+
+To run  
+(Main class just performs _"4. Get summary of games by total score [...]"_ test case from exercise description)
+```
+./gradlew run
+```
+
+## Technologies used
+```
+JDK Azul Zulu@21.0.4
+gradle (wrapper version)
+Spock 
+```
--- src/main/java/com/sr/Main.java
+++ src/main/java/com/sr/Main.java
@@ -1,8 +1,27 @@
 package com.sr;
 
+import com.sr.scoreboard.Scoreboard;
+import com.sr.scoreboard.WorldCupScoreboard;
+
 public class Main {
 
   public static void main(String[] args) {
+    final Scoreboard wc = new WorldCupScoreboard();
+    wc.startGame("Mexico", "Canada");
+    wc.updateScore("Mexico", "Canada", 0, 5);
 
+    wc.startGame("Spain", "Brazil");
+    wc.updateScore("Spain", "Brazil", 10, 2);
+
+    wc.startGame("Germany", "France");
+    wc.updateScore("Germany", "France", 2, 2);
+
+    wc.startGame("Uruguay", "Italy");
+    wc.updateScore("Uruguay", "Italy", 6, 6);
+
+    wc.startGame("Argentina", "Australia");
+    wc.updateScore("Argentina", "Australia", 3, 1);
+
+    wc.getSummary().forEach(System.out::println);
   }
 }
